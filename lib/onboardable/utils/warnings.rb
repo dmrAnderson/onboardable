72fcca6852568d24eb9062d6069c4cf900c76d9a
Artem Skrynnyk <skrynnyk.artem@coaxsoft.com>
2024-04-19 11:44:11 +0300

    private warn_about_duplicates

--- lib/onboardable/utils/warnings.rb
+++ lib/onboardable/utils/warnings.rb
@@ -3,6 +3,8 @@
 module Onboardable
   module Utils
     module Warnings
+      private
+
       def warn_about_duplicates(new_steps)
         duplicated_steps = new_steps.tally.select { |_, occurrences| occurrences.size > 1 }.keys
         warn("Ignored duplicates: `#{duplicated_steps.join('`, `')}`.", uplevel: 1)
